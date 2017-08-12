5d7bb366a5da872ef46f8075bccbebecabe739d8
永宮正陽 <MASAHARU@MASAHARUBook-Air.local>
2017-08-12 11:32:21 +0900

    first

--- ARTwitter/ViewController.swift
+++ ARTwitter/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("hello world")
+        print("hello world!!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
