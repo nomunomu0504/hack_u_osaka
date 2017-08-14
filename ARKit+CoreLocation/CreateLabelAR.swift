499f7445f5be77046effd10fd74876f395c83221
[masashi] <[swim2000616@gmail.com]>
2017-08-14 23:57:11 +0900

    ar上にSCNNodeを使ってテキスト表示は可能にlocationNodeでは表示できない

--- ARKit+CoreLocation/CreateLabelAR.swift
+++ ARKit+CoreLocation/CreateLabelAR.swift
@@ -17,62 +17,78 @@ open class CreateLabelAR: LocationNode {
 
     
     
-    public let annotationNode: SCNNode
-    
-    public let arLabel: SCNText
-    
-//    public let floor: SCNBox
-
-    
     public var scaleRelativeToDistance = false
     
     
     
     public init(location: CLLocation?, image: UIImage) {
         
-//        let plane = SCNPlane(width: 10000000, height: 10000000)
-////        plane.firstMaterial!.diffuse.contents = image
-//        plane.firstMaterial!.lightingModel = .constant
-//
-//
-//
-//        self.floor = SCNBox(width: CGFloat(10000), height: 0.0001, length: CGFloat(10000), chamferRadius: 0.0)
-//
-//        let color: SCNMaterial = SCNMaterial()
-//        color.diffuse.contents = UIColor.red
-//        color.lightingModel = .constant
-//
-//        plane.materials = [color]
-//
-//
-//        self.floor.materials = [color]
-//
-////        let boxShape: SCNPhysicsShape = SCNPhysicsShape(geometry: self.floor, options: nil)
-//        let boxNode: SCNNode = SCNNode(geometry: self.floor)
-//
-//
+        
+        
+        
+        // SCNText
+        let text = SCNText()
+        
+        //        text.containerFrame = CGRect(x: -0.5, y: -2, width: 2, height: 4) // 幅を決める
+        
+        //text.truncationMode = kCATruncationEnd
+        //text.alignmentMode = kCAAlignmentCenter
+        //text.font = UIFont(name: "Futura-Bold", size: 0.5)
+        
+        // NSAttributedString 設定
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = -0.5
+        
+        let textFont:UIFont = UIFont(name: "Futura-Bold", size: 0.5)!
+        
+        
+        text.string = NSAttributedString(string: "Hello World!!!", attributes:
+            [NSAttributedStringKey.paragraphStyle: style,
+             NSAttributedStringKey.font: textFont]
+        )
+        
+        
+        // その他の設定
+        text.flatness = 0.0
+        text.extrusionDepth = 0.1 // テキストの奥行きの幅を設定　デフォルト1
+        text.chamferRadius = 0.02
+        
+        
+        // chamfer の形状設定
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0.0, y: 1.0))
+        path.addLine(to: CGPoint(x: 0.0, y: 0.75))
+        path.addLine(to: CGPoint(x: 0.25, y: 0.75))
+        path.addLine(to: CGPoint(x: 0.25, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.25))
+        path.addLine(to: CGPoint(x: 0.75, y: 0.25))
+        path.addLine(to: CGPoint(x: 0.75, y: 0.0))
+        path.addLine(to: CGPoint(x: 1.0, y: 0.0))
+        
+        text.chamferProfile = path
+        
+        // マテリアル設定
+        
+        //        let m1 = SCNMaterial()
+        //        let m2 = SCNMaterial()
+        //        let m3 = SCNMaterial()
+        //
+        //        m1.diffuse.contents = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+        //        m2.diffuse.contents = UIColor.red
+        //        m3.diffuse.contents = UIColor.yellow
+        //
+        //        text.materials = [m3,m2,m1,m2,m3]
+        
+        text.firstMaterial!.diffuse.contents = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+        
+        let node = SCNNode(geometry: text)
+        node.position = SCNVector3Zero
+        
+        
+        node.scale = SCNVector3(x: 10, y: 10, z: 10) //SCN Nodeのscaleを変更
 
         
-        
-        
-        // ボタンのサイズを定義.
-        let bWidth: CGFloat = 100000
-        let bHeight: CGFloat = 100000
-        // 配置する座標を定義(画面の中心).
-//      let posX: CGFloat = self.view.bounds.width/2 - bWidth/2
-//      let posY: CGFloat = self.view.bounds.height/2 - bHeight/2
-
-        
-        arLabel = SCNText()
-        arLabel.string = "hello"
-        arLabel.font = UIFont.systemFont(ofSize: CGFloat(20000))
-        arLabel.containerFrame =  CGRect(x: 0, y: 0, width: image.size.width / 100, height: image.size.width / 100)
-        
-        
-        arLabel.firstMaterial!.lightingModel = .constant
-        
-        annotationNode = SCNNode()
-        annotationNode.geometry = arLabel
         
         
         
@@ -83,7 +99,10 @@ open class CreateLabelAR: LocationNode {
         billboardConstraint.freeAxes = SCNBillboardAxis.Y
         constraints = [billboardConstraint]
         
-        addChildNode(annotationNode)
+//        addChildNode(annotationNode)
+        
+        addChildNode(node)
+
         
 
 
--- ARKit+CoreLocation/ViewController.swift
+++ ARKit+CoreLocation/ViewController.swift
@@ -74,6 +74,82 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
         
         sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: pinLocationNode)
        
+        let labelLocationNode = CreateLabelAR(location: nil, image: pinImage)
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: labelLocationNode)
+        
+        
+        
+//        (latitude: 35.938256655652317, longitude: 136.18172222693536)
+        
+        
+        
+        
+        // SCNText
+        let text = SCNText()
+        
+        //        text.containerFrame = CGRect(x: -0.5, y: -2, width: 2, height: 4) // 幅を決める
+        
+        //text.truncationMode = kCATruncationEnd
+        //text.alignmentMode = kCAAlignmentCenter
+        //text.font = UIFont(name: "Futura-Bold", size: 0.5)
+        
+        // NSAttributedString 設定
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = -0.5
+        
+        let textFont:UIFont = UIFont(name: "Futura-Bold", size: 0.5)!
+        
+        
+        text.string = NSAttributedString(string: "Hello World!!!", attributes:
+            [NSAttributedStringKey.paragraphStyle: style,
+             NSAttributedStringKey.font: textFont]
+        )
+        
+        
+        // その他の設定
+        text.flatness = 0.0
+        text.extrusionDepth = 0.1 // テキストの奥行きの幅を設定　デフォルト1
+        text.chamferRadius = 0.02
+        
+        
+        // chamfer の形状設定
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0.0, y: 1.0))
+        path.addLine(to: CGPoint(x: 0.0, y: 0.75))
+        path.addLine(to: CGPoint(x: 0.25, y: 0.75))
+        path.addLine(to: CGPoint(x: 0.25, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.5))
+        path.addLine(to: CGPoint(x: 0.5, y: 0.25))
+        path.addLine(to: CGPoint(x: 0.75, y: 0.25))
+        path.addLine(to: CGPoint(x: 0.75, y: 0.0))
+        path.addLine(to: CGPoint(x: 1.0, y: 0.0))
+        
+        text.chamferProfile = path
+        
+        // マテリアル設定
+        
+        //        let m1 = SCNMaterial()
+        //        let m2 = SCNMaterial()
+        //        let m3 = SCNMaterial()
+        //
+        //        m1.diffuse.contents = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+        //        m2.diffuse.contents = UIColor.red
+        //        m3.diffuse.contents = UIColor.yellow
+        //
+        //        text.materials = [m3,m2,m1,m2,m3]
+        
+        text.firstMaterial!.diffuse.contents = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+        
+        let node = SCNNode(geometry: text)
+        node.position = SCNVector3Zero
+        
+        
+        node.scale = SCNVector3(x: 0.1, y: 0.1, z: 0.1) //SCN Nodeのscaleを変更
+        
+        sceneLocationView.scene.rootNode.addChildNode(node)
+
+        
         
         view.addSubview(sceneLocationView)
         
@@ -323,6 +399,8 @@ extension UIView {
     }
 
 }
+
+
 
 //public extension LocationAnnotationNode {
 //
