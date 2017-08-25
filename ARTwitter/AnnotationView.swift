23eda6dfd9b45f8dd6c59bc2c8e9accff2c44ffc
masaharun <masaharu@nken.main.jp>
2017-08-25 12:54:13 +0900

    arのui改善

--- ARTwitter/AnnotationView.swift
+++ ARTwitter/AnnotationView.swift
@@ -45,14 +45,31 @@ class AnnotationView: ARAnnotationView {
     let label = UILabel(frame: CGRect(x: 10, y: 0, width: self.frame.size.width, height: 30))
     label.font = UIFont.systemFont(ofSize: 16)
     label.numberOfLines = 0
-    label.backgroundColor = UIColor(white: 0.3, alpha: 0.7)
+    label.backgroundColor = UIColor(white: 0.3, alpha: 0.7) //0.3
+    
+//    label.backgroundColor = UIColor.white
     label.textColor = UIColor.white
+//    label.layer.bounds.width = 2.0
+//    label.layer.bounds.width =
+    
+    
+    label.layer.borderWidth = 0.5
+    
+//    label.layer.borderColor = UIColor.blue.cgColor
+    label.layer.cornerRadius = 4
+    label.layer.masksToBounds = true
+    
+    
     self.addSubview(label)
     self.titleLabel = label
     
     distanceLabel = UILabel(frame: CGRect(x: 10, y: 30, width: self.frame.size.width, height: 20))
     distanceLabel?.backgroundColor = UIColor(white: 0.3, alpha: 0.7)
-    distanceLabel?.textColor = UIColor.blue
+    distanceLabel?.textColor = UIColor.orange
+    
+
+    
+    
     distanceLabel?.font = UIFont.systemFont(ofSize: 12)
     self.addSubview(distanceLabel!)
     
